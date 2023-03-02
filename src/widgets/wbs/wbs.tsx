40c4ce2f4e775f553714f45cd3e57659bc92b0d7
Kirilenko <>
2023-03-02 20:24:20 +0400

    feat: add timestamp

--- src/widgets/wbs/wbs.tsx
+++ src/widgets/wbs/wbs.tsx
@@ -5,11 +5,16 @@ import { DemoButton, DemoHeader } from '@shared/ui'
 import StyledWbs from './wbs.style'
 
 const Wbs: FC = () => (
-  <StyledWbs>
-    <DemoHeader title="shared.ui.header in widgets.wbs" />
-    <DemoButton title="shared.ui.button in widgets.wbs" />
-    <p>remote.widgets.wbs.host</p>
-  </StyledWbs>
+  <>
+    <StyledWbs>
+      <DemoHeader title="shared.ui.header in widgets.wbs" />
+      <DemoButton title="shared.ui.button in widgets.wbs" />
+      <p>remote.widgets.wbs.host</p>
+    </StyledWbs>
+    <div style={{ display: 'none' }}>
+      {`widgets.wbs: ${import.meta.env.VITE_TIMESTAMP}`}
+    </div>
+  </>
 )
 
 export default Wbs
